d971e45f06c62479df54506c449031e7cc04010c
Biplob45 <mdbadiuzzamanbiplob@gmail.com>
2018-06-03 02:47:04 +0600

    update, delete and validation check

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
 
-
+validates :title, presence: true, length:{minimun:4, maximum:40}
+validates :description, presence: true, length:{minimun:20, maximum:500}
 
 end
