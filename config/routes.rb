98e815589817ece0959486bf8156a8e37e8296eb
Biplob45 <mdbadiuzzamanbiplob@gmail.com>
2018-06-03 01:29:34 +0600

    create a project name railscrud, also create a controller name page and two file about and home

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #get 'page/home', to:'page#home'
+  root to:'page#home'
+
+  get 'page/about', to:'page#about'
+
+end
